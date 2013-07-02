ed7df28067a8812ea7c5d1fd6ebfe13eedadf065
tssge <tssge93@gmail.com>
2013-07-02 16:40:57 +0300

    1.6

--- src/fi/sweetmoon/moonifier/Moonifier.java
+++ src/fi/sweetmoon/moonifier/Moonifier.java
@@ -3,7 +3,7 @@ package fi.sweetmoon.moonifier;
 import java.util.HashMap;
 import java.util.List;
 
-import org.bukkit.craftbukkit.v1_5_R3.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_6_R1.entity.CraftLivingEntity;
 
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
